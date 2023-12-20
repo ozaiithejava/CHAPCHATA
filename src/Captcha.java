c915acea432a428345d0fec236fd1b750c1aa66c
yigitthejava <82784013+ozaiithejava@users.noreply.github.com>
2023-12-20 19:56:03 +0300

    Create Captcha.java

---
+++ src/Captcha.java
@@ -0,0 +1,12 @@
+
+public class Captcha {
+    private String value;
+
+    public Captcha(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
